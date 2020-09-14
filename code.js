cab24965c90a524aa36e5ac7ab7861e8f811e04c
Mohammad sobuj <mohammad@sobuj.gov>
2020-09-15 02:42:47 +0600

    this is my love

--- code.js
+++ code.js
@@ -2,3 +2,44 @@ console.log('sobuj')
 
 var ria="Ria I love You so much"
 console.log(ria)
+var Obj={
+    name:'sobuj',
+    age:23,
+    student:'university',
+    Dpartment:'CSE',
+    fsub:'english + math',
+    Gfriend:'Ria hurt',
+    Ryear:'one time'
+
+
+}
+console.log(Obj)
+
+function love(){
+    for( ; ; ){
+        var ran=Math.round(Math.random()*100+1)
+        if(ran>79 && ran<85){
+            console.log(ran+'%'+' Ria cool')
+            break;
+        
+        }
+        else  if(ran>85 && ran<90){
+            console.log(ran+'%'+' Nusrat moon')
+            break;
+        
+        }
+        else  if(ran>90 && ran<95){
+            console.log(ran+'%'+' Sinthia hot')
+            break;
+        
+        }
+        else  if(ran>95 && ran<100){
+            console.log(ran+'%'+' Afsana hurt')
+            break;
+        
+        }
+    }
+}
+love();
+
+
